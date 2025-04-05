784f1649549810f4a8cd12b6bec4eea9e457a20a
duck4i <mihailogazda@gmail.com>
2025-04-05 16:03:17 +0200

    cleanup

--- st7262/main/main.c
+++ st7262/main/main.c
@@ -9,9 +9,13 @@
 #include <esp_heap_caps.h>
 #include <esp_lcd_st7262.h>
 
-// #define LVGL_PORT 1
 #define USE_LVGL 1
+//#define USE_LVGL_PORT 1
 // #define TEST_FULL_SCREEN 1
+
+#define STACK_SIZE 8192
+#define TASK_PRIORITY 9
+
 #define TAG "ESP32-MAIN"
 
 #ifdef USE_LVGL
@@ -30,7 +34,7 @@ static uint32_t esp_tick(void)
     return esp_timer_get_time() / 1000;
 }
 
-void setup_lvgl(uint32_t width, uint32_t height, esp_lcd_panel_st7262_panel_handle_t panel)
+static void setup_lvgl(uint32_t width, uint32_t height, esp_lcd_panel_st7262_panel_handle_t panel)
 {
     ESP_LOGI(TAG, "Setting up LVGL...");
 
@@ -44,7 +48,7 @@ void setup_lvgl(uint32_t width, uint32_t height, esp_lcd_panel_st7262_panel_hand
     lv_display_set_color_format(disp_handle, LV_COLOR_FORMAT_RGB565);
 
     size_t size = width * height * sizeof(lv_color16_t) / 4;
-    lv_color16_t *draw_buf = (lv_color16_t *)heap_caps_malloc(size, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+    lv_color16_t *draw_buf = (lv_color16_t *)heap_caps_malloc(size, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
     if (draw_buf == NULL)
     {
         ESP_LOGE(TAG, "Could not allocate draw buffer memory");
@@ -55,6 +59,65 @@ void setup_lvgl(uint32_t width, uint32_t height, esp_lcd_panel_st7262_panel_hand
 
     lv_demo_widgets();
     lv_demo_widgets_start_slideshow();
+
+    ESP_LOGI(TAG, "LVGL Demo started.");
+}
+
+#endif
+
+#ifdef USE_LVGL_PORT
+#include <esp_lvgl_port.h>
+#include <lv_demos.h>
+
+static void setup_lvgl_port(uint32_t width, uint32_t height, esp_lcd_panel_st7262_panel_handle_t panel)
+{
+    ESP_LOGI(TAG, "Setting up LVGL port...");
+
+    const lvgl_port_cfg_t lvgl_port_config = {
+        .task_affinity = 1,
+        .task_stack = STACK_SIZE,
+        .task_max_sleep_ms = 500,
+        .timer_period_ms = 5,
+        .task_priority = TASK_PRIORITY,
+    };
+
+    esp_err_t error = lvgl_port_init(&lvgl_port_config);
+    if (error != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to initialize LVGL port: %s", esp_err_to_name(error));
+        return;
+    }
+
+    const lvgl_port_display_cfg_t display_config = {
+        .panel_handle = panel->handle,
+        .hres = width,
+        .vres = height,
+        .color_format = LV_COLOR_FORMAT_RGB565,
+        .buffer_size = width * height * sizeof(lv_color16_t) / 4,
+        .monochrome = false,
+        .flags = {
+            .direct_mode = false,
+            .buff_spiram = true,
+            .buff_dma = false,
+        }};
+
+    const lvgl_port_display_rgb_cfg_t rgb_display_config = {
+        .flags = {
+            .bb_mode = false,
+            .avoid_tearing = false,
+        }};
+
+    lv_display_t *display_handle = lvgl_port_add_disp_rgb(&display_config, &rgb_display_config);
+    if (display_handle == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to add LVGL display");
+        return;
+    }
+
+    lvgl_port_lock(0);
+    lv_demo_widgets();
+    lv_demo_widgets_start_slideshow();
+    lvgl_port_unlock();
 
     ESP_LOGI(TAG, "LVGL Demo started.");
 }
@@ -118,6 +181,8 @@ void main_task(void *parg)
         lv_timer_handler();
         vTaskDelay(pdMS_TO_TICKS(5));
     }
+#elif USE_LVGL_PORT
+    setup_lvgl_port(panel_config.width, panel_config.height, &panel);
 #else
     while (true)
     {
@@ -145,6 +210,10 @@ void app_main(void)
     ESP_LOGI(TAG, "Free internal heap: %u bytes", heap_caps_get_free_size(MALLOC_CAP_INTERNAL));
     ESP_LOGI(TAG, "Free PSRAM: %u bytes", heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
 
+#ifndef USE_LVGL_PORT
     TaskHandle_t main_task_handle = NULL;
-    xTaskCreate(main_task, "main_task", 9064, NULL, 5, &main_task_handle);
+    xTaskCreate(main_task, "main_task", STACK_SIZE, NULL, TASK_PRIORITY, &main_task_handle);
+#else
+    main_task(NULL);
+#endif
 }
